2c0e7b389f0c85c89d93b5947dc8bae541793968
Jeremiah Lewis <4462211+jlewis91@users.noreploy.github.com>
2019-07-09 20:34:33 +0200

    Relax package requirements to minor version

--- setup.py
+++ setup.py
@@ -4,15 +4,16 @@ from io import open
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.rst'), encoding='UTF-8').read()
+README = open(os.path.join(here, "README.rst"), encoding="UTF-8").read()
 
-setup(name='dwdweather2',
-      version='0.9.0',
-      description='Python client to access weather data from Deutscher Wetterdienst (DWD), '
-                  'the federal meteorological service in Germany.',
-      long_description=README,
-      license="MIT",
-      classifiers=[
+setup(
+    name="dwdweather2",
+    version="0.9.0",
+    description="Python client to access weather data from Deutscher Wetterdienst (DWD), "
+    "the federal meteorological service in Germany.",
+    long_description=README,
+    license="MIT",
+    classifiers=[
         "Programming Language :: Python",
         "License :: OSI Approved :: MIT License",
         "Development Status :: 4 - Beta",
@@ -39,32 +40,28 @@ setup(name='dwdweather2',
         "Topic :: Utilities",
         "Operating System :: POSIX",
         "Operating System :: Unix",
-        "Operating System :: MacOS"
-      ],
-      author='Marian Steinbach',
-      author_email='marian@sendung.de',
-      url='https://github.com/hiveeyes/dwdweather2',
-      keywords='dwd cdc deutscher wetterdienst climate data center weather '
-               'opendata data acquisition transformation export '
-               'geospatial temporal timeseries '
-               'sensor network observation '
-               'http rest api '
-               'json csv'
-               'rdbms sql sqlite '
-               'grafana',
-      packages=find_packages(),
-      include_package_data=True,
-      install_requires=[
-          'tqdm==4.32.1',
-          'python-dateutil==2.8.0',
-          'requests==2.22.0',
-          'requests-cache==0.5.0',
-          #'htmllistparse==0.5',     # Needs min. Python 3.3
-          'beautifulsoup4==4.7.1',
-      ],
-      entry_points={
-          'console_scripts': [
-              'dwdweather = dwdweather.commands:run'
-          ]
-      }
+        "Operating System :: MacOS",
+    ],
+    author="Marian Steinbach",
+    author_email="marian@sendung.de",
+    url="https://github.com/hiveeyes/dwdweather2",
+    keywords="dwd cdc deutscher wetterdienst climate data center weather "
+    "opendata data acquisition transformation export "
+    "geospatial temporal timeseries "
+    "sensor network observation "
+    "http rest api "
+    "json csv"
+    "rdbms sql sqlite "
+    "grafana",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "tqdm>=4.32,<4.33",
+        "python-dateutil>=2.8,<2.9",
+        "requests>=2.22,<2.23",
+        "requests-cache>=0.5,<0.6",
+        #'htmllistparse==0.5',     # Needs min. Python 3.3
+        "beautifulsoup4>=4.7,<4.8",
+    ],
+    entry_points={"console_scripts": ["dwdweather = dwdweather.commands:run"]},
 )
